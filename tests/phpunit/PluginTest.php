1f2b7294d5ff1fcf4b2626bc4692eb2dc2bd2767
Matthew Grasmick <matt.grasmick@gmail.com>
2018-01-31 17:18:39 -0500

    Fix plugin test.

--- tests/phpunit/PluginTest.php
+++ tests/phpunit/PluginTest.php
@@ -25,7 +25,7 @@ class PluginTest extends CommandTestBase
     public function testComposerCommandsAvailable($expected)
     {
         // Code executed in `composer list` is not counted under code coverage.
-        $process = new Process('COMPOSER_ALLOW_XDEBUG=1 composer list', $this->sandbox);
+        $process = new Process('COMPOSER_ALLOW_XDEBUG=1 ./vendor/bin/composer list', $this->sandbox);
         $process->run();
         $this->assertContains($expected, $process->getOutput());
     }
