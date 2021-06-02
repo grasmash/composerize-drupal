a995ab9dc603fdfbbab4d668e6b9ab387470c542
Rahul Sonar <74297357+rahulsonar-acquia@users.noreply.github.com>
2021-06-03 00:58:03 +0530

    Upgrade to composer2 | Fix unit test cases (#32)

--- src/Composer/Plugin.php
+++ src/Composer/Plugin.php
@@ -19,6 +19,14 @@ class Plugin implements PluginInterface, Capable
         $this->io = $io;
     }
 
+    public function deactivate(Composer $composer, IOInterface $io)
+    {
+    }
+
+    public function uninstall(Composer $composer, IOInterface $io)
+    {
+    }
+
     public function getCapabilities()
     {
         return array(
